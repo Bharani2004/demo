d6e5e4eafcdb08fe71231c763f02c1f360b370c3
Bharani2004 <bharanikumarbk2004@gmail.com>
2024-07-08 21:35:21 +0530

    Routes

---
+++ server/routes/transactionRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const { addTransaction, getAllTransaction, editTransaction, deleteTransaction } = require('../controllers/transactionCtrl')
+
+
+//router
+
+const router = express.Router()
+
+//add transaction POST
+router.post("/add-transaction", addTransaction)
+
+router.post("/edit-transaction", editTransaction)
+
+router.post('/delete-transaction', deleteTransaction)
+
+//get transaction
+router.post('/get-transaction', getAllTransaction)
+
+
+
+module.exports=router
